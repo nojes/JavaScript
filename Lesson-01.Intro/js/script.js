dd560c7c4784bdd847245aa7e392d7da10ccb3e6
nojes <vv.nojenko@gmail.com>
2016-05-17 11:43:58 +0300

    1st commit // take 3

---
+++ Lesson-01.Intro/js/script.js
@@ -0,0 +1,15 @@
+console.log("Hello, Console Log");
+//alert("Hello, Alert");
+
+var fname = "Vyacheslav";
+var lname = "Nozhenko";
+var age = 19;
+var float_age = 19.3;
+var developer = true;
+
+
+console.log(fname);
+console.log(lname);
+console.log(age);
+console.log(float_age);
+console.log(developer);
