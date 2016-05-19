047d369eb449e629afaf60e93964c91411d21a8e
Vyacheslav <vv.nojenko@gmail.com>
2016-05-19 18:32:04 +0300

    fix bugs + little makeup

--- Lesson.02-Functions&Objects/js/script-ex2.js
+++ Lesson.02-Functions&Objects/js/script-ex2.js
@@ -1,5 +1,20 @@
 function showCalc(a, b, operation, res) {
-    console.log(a + " " + operation + " " + b + " = " + res);
+    document.write(a + " " + operation + " " + b + " = " + res);
+}
+
+function Max(a, b) {
+    return (a > b) ? a : b;
+}
+function Min(a, b) {
+    return (a > b) ? b : a;
+}
+
+function Pow(val, n) {
+    var sum = val;
+    for(var i = 0; i < n - 1; i++) {
+        sum *= val;
+    }
+    return sum;
 }
 
 function Calc(a, b, operation) {
@@ -19,52 +34,22 @@ function Calc(a, b, operation) {
             res = a / b;
             break;
         case '^':
-            var sum = b;
-            for(var i = 0; i < b - 1; i++) {
-                sum *= b;
-            }
-            res = sum;
-            break;
-
-        case 'max':
-            res = (a > b) ? a : b;
-            break;
-        case 'min':
-            res = (a < b) ? a : b;
+            res = Pow(a, b);
             break;
         }
     return res;
-}
-
-function Max(a, b) {
-    return (a > b) ? a : b;
-}
-
-function Pow(val, n) {
-    var sum = val;
-    for(var i = 0; i < n - 1; i++) {
-        sum *= val;
-    }
-    return sum;
 }
 
 var a = parseInt(prompt("Put a"), a);
 var b = parseInt(prompt("Put b"), b);
 var operation = prompt("Operation");
 
-console.log(operation);
 showCalc(a, b, operation, Calc(a, b, operation));
 
-//console.log("Max: " + Max(a, b));
-
-//var n = parseInt(prompt("Put pow"), n);
-//console.log("Pow: " + Pow(a, n));
-
-
-var obj = {
-    cars: [
-        "bmw", "renault", "citroen", "audi"
-    ]
-}
-
-console.log(obj.cars);
+//
+//var obj = {
+//    cars: [
+//        "bmw", "renault", "citroen", "audi"
+//    ]
+//}
+//document.write("<br>" + obj.cars);
