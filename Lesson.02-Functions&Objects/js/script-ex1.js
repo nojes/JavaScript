a438057a5235828d36a146e337913119cf393d26
Vyacheslav <vv.nojenko@gmail.com>
2016-05-19 10:39:09 +0300

    discount constants

--- Lesson.02-Functions&Objects/js/script-ex1.js
+++ Lesson.02-Functions&Objects/js/script-ex1.js
@@ -4,21 +4,21 @@
 
 // Variables
 var discount = 0;
-var month_pay = 1000;
 var sum = 0;
+var month_pay = 1000;
 var studied = confirm("You have studied?");
 var with_friend = confirm("You came with a friend?");
 var full_pay = confirm("You want to pay the full course?");
 
 // Discounts
-var dis_per_studied = 5;
-var dis_per_with_friend = 10;
-var dis_per_full_pay = 10;
+var DIS_STUDIED = 5;
+var DIS_WITH_FRIEND = 10;
+var DIS_FULL_PAY = 10;
 
-// Discount calc
-(studied) ? discount = dis_per_studied : 0;
-(with_friend) ? discount += dis_per_with_friend : 0;
-(full_pay) ? discount += dis_per_full_pay : 0;
+// Calc
+(studied) ? discount += DIS_STUDIED : 0;
+(with_friend) ? discount += DIS_WITH_FRIEND : 0;
+(full_pay) ? discount += DIS_FULL_PAY : 0;
 sum = month_pay - (month_pay * discount / 100);
 
 // Output
