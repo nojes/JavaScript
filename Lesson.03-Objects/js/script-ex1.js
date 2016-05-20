f1dc1ecad3d101898d3f0f27b52dacd1d300e420
Vyacheslav <vv.nojenko@gmail.com>
2016-05-20 10:18:55 +0300

    simple actions with object

--- Lesson.03-Objects/js/script-ex1.js
+++ Lesson.03-Objects/js/script-ex1.js
@@ -1,4 +1,43 @@
 /**
- * Example 1 - "Discounts for education"
+ *  Example 1 - "Simple object"
  */
 
+function ShowVar(variable, title) {
+    document.write(title + ": " + variable + "<br>");
+}
+
+// Object
+var person = {
+    firstname: "Vyacheslav",
+    lastname: "Nozhenko",
+    age: 19
+}
+
+// Methods
+person.getAge = function() {
+    return person.age;
+}
+person.getFirstname = function() {
+    return person.firstname;
+}
+
+person.Display = function() {
+   document.write("<br>");
+    ShowVar(person.firstname, "Firstname");
+    ShowVar(person.lastname, "Lastname");
+    ShowVar(person.age, "Age");
+}
+
+// add new field
+person.email = "vv.nojenko@gmail.com";
+
+person.Display();
+person.age++;
+
+person.Display();
+ShowVar(person.email, "Email");
+
+// delete field
+delete person.email;
+person.Display();
+ShowVar(person.email, "Email");
