a0bac436d3216b7caecc0f9afa50e1d3d298dd80
Nojes <vv.nojenko@gmail.com>
2016-05-24 10:30:52 +0300

    Date methods

--- Lesson.04-Date,Math/js/script-ex1.js
+++ Lesson.04-Date,Math/js/script-ex1.js
@@ -1,4 +1,28 @@
 /**
- *  Example 1 - ""
+ *  Example 1 - "Date"
  */
 
+function ShowVar(title, variable) {
+    document.write("<br>" + title + ": " + variable + "<br>");
+}
+
+var date = new Date();
+
+ShowVar("Date()", Date());
+
+// return [1-31]
+ShowVar("getDate()", date.getDate());
+// return [1-31]
+ShowVar("getDay()", date.getDay());
+// return [1-12]
+ShowVar("getMonth()", date.getMonth());
+// return [1970-..]
+ShowVar("getFullYear()", date.getFullYear());
+// return [0-23]
+ShowVar("getHours()", date.getHours());
+// return [0-59]
+ShowVar("getMinutes()", date.getMinutes());
+// return [0-59]
+ShowVar("getSeconds()", date.getSeconds());
+// return [0-1000]
+ShowVar("getMilliseconds()", date.getMilliseconds());
