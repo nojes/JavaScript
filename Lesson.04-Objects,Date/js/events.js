4c8e7fe7258021db867fa81b362262f5a1ed57a2
Nojes <vv.nojenko@gmail.com>
2016-05-26 09:21:41 +0300

    event functions

---
+++ Lesson.04-Objects,Date/js/events.js
@@ -0,0 +1,11 @@
+function leftClick() {
+    alert('Вы кликнули левой кнопкой');
+}
+
+function rightClick() {
+    alert('Вы кликнули правой кнопкой')
+}
+
+function focusMouse() {
+    alert('Вы навели мышкой')
+}
