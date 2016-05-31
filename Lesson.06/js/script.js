d6d50b90f8f44d7d94d62a9e1a8c0f593dce6396
Nojes <vv.nojenko@gmail.com>
2016-05-31 10:55:07 +0300

    some events example

---
+++ Lesson.06/js/script.js
@@ -0,0 +1,37 @@
+/**
+ *  @author nojes <vv.nojenko@gmail.com>
+ */
+
+// Add Event Listeners
+document.getElementById("one").addEventListener("click", onClick);
+document.getElementById("two").addEventListener("contextmenu", onRightClick);
+document.getElementById("three").addEventListener("mouseover", onMouseOver);
+document.getElementById("four").addEventListener("mousewheel", onScroll);
+document.getElementById("five").addEventListener("mouseover", onMouseWhell);
+
+// Functions
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Events functions
+function onClick() {
+    event.target.style.backgroundColor = getRandomColor();
+}
+function onRightClick() {
+    event.target.style.backgroundColor = getRandomColor();
+}
+function onMouseOver() {
+    event.target.style.backgroundColor = getRandomColor();
+}
+function onScroll() {
+    event.target.style.backgroundColor = getRandomColor();
+}
+function onMouseWhell() {
+    event.target.style.backgroundColor = getRandomColor();
+}
